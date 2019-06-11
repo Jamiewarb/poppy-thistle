6457f966eb3a219fdfb102a42a6e1612013ad48d
Jamie Warburton <jamie@hexdigital.com>
2019-06-11 11:11:36 +0100

    Add a stationery page

---
+++ pages/stationery.vue
@@ -0,0 +1,17 @@
+<template>
+  <Wrapper>
+    <span>Stationery Page</span>
+  </Wrapper>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Wrapper from '~/components/Layout/Wrapper.vue'
+
+@Component({
+  components: {
+    Wrapper
+  }
+})
+export default class StationeryPage extends Vue {}
+</script>
